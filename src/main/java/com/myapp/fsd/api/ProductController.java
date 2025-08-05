3997345d76f1d069a4ceb5628f79fe148bf34a17
pradeepkrishnamurthy <pradeeepmurthy@gmail.com>
2025-08-05 20:04:48 +0530

    Product Update inside the rest api

--- src/main/java/com/myapp/fsd/api/ProductController.java
+++ src/main/java/com/myapp/fsd/api/ProductController.java
@@ -3,10 +3,7 @@ package com.myapp.fsd.api;
 import com.myapp.fsd.domain.Product;
 import com.myapp.fsd.repository.ProductRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +23,24 @@ public class ProductController {
     @GetMapping("{id}")
     public ResponseEntity<Product> getProduct(@PathVariable Integer   id) {
         return ResponseEntity.ok(repository.findById(id).get());
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(
+            @PathVariable Integer id,
+            @RequestBody Product updatedProduct
+    ) {
+        return repository.findById(id)
+                .map(existingProduct -> {
+                    // Update fields
+                    existingProduct.setName(updatedProduct.getName());
+                    existingProduct.setPrice(updatedProduct.getPrice());
+                    existingProduct.setDescription(updatedProduct.getDescription());
+                    existingProduct.setProductAvailable(updatedProduct.isProductAvailable());
+                    existingProduct.setStarRating(updatedProduct.getStarRating());
+                    existingProduct.setImageUrl(updatedProduct.getImageUrl());
+                    repository.save(existingProduct);
+                    return ResponseEntity.ok(existingProduct);
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
